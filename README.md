04fc8cb6c6f0dc1057a2e36d01d8ec8639fb93dd
MoonshineSG <ovidiu@binary.local>
2016-01-08 17:04:13 +0800

    allow settings in the command line not just ini file

--- README.md
+++ README.md
@@ -1,12 +1,5 @@
 For OSX only. With changes, might be usefull on other platforms as well.
 
-
-Add this to post procesing script in Simplify3D
-
-`"/path/to/toctoprint.py" --gcode "[output_filepath]"`
-
-![screenshot](screenshot_1.png)
-
 Install
 
 ```
@@ -16,20 +9,45 @@ sudo easy_install python-daemon
 sudo easy_install configparser
 ```
 
-create `.toctoprint.ini` in your home folder
+Except for the parameter --gcode all options can be set in `ini` file
+
+You can create `.toctoprint.ini` in your home folder with any (or all) following settings or pass as command line parameters
 
 ```
 [default]
-\#Octoprint setting
-SERVER = http://X.X.X.X
-OCTOPRINT_KEY = XXXXXX
+#Octoprint setting
+#--server (default http://octoprint.local)
+SERVER = http://octoprint.local
+#--key (default 'none' - this has to be set either in the `ini` file or passed as command line parameter)
+OCTOPRINT_KEY = 00000000000000000000000
 
-\#only gcode files created to this folder will be uploaded
+#only gcode files created to this folder will be uploaded
+#--location 
 DEFAULT_LOCATION = ~/Desktop
 
-\#change this to None if you don't want to open when saving to a different path
+#change this to None if you don't want to open when saving to a different path
+#--editor
 EDITOR = /usr/local/bin/mate
 
-\#remove local gcode file after uploading to Octoprint
+#remove local gcode file after uploading to Octoprint
+#trash
 TRASH = True
+
+#select file after upload
+#select
+SELECT = False 
+
+#start print after upload
+#print
+PRINT = False 
 ```
+
+
+Add this to post procesing script in Simplify3D
+
+`"/path/to/toctoprint.py" select trash --location ~/Desktop --editor /usr/local/bin/mate --key 00000000000000000 --server http://octoprint.local --gcode [output_filepath]`
+
+Settings passed as parameters overwrite the ones in the ini file
+
+![screenshot](screenshot_1.png)
+
--- toctoprint.py
+++ toctoprint.py
@@ -21,7 +21,7 @@ def upload(gcode):
 	try:
 		name = os.path.basename(gcode)
 		notify("Uploading '%s' GCODE to Octoprint..."%name)
-		ret = call(["/usr/bin/curl", "--connect-timeout", "15" ,"-H", "Content-Type: multipart/form-data", "-H", "X-Api-Key: {0}".format(OCTOPRINT_KEY), "-F", "select=true", "-F", "print=false", "-F", "file=@{0}".format(gcode), "{0}/api/files/local".format(SERVER)])
+		ret = call(["/usr/bin/curl", "--connect-timeout", "15" ,"-H", "Content-Type: multipart/form-data", "-H", "X-Api-Key: {0}".format(OCTOPRINT_KEY), "-F", SELECT, "-F", PRINT, "-F", "file=@{0}".format(gcode), "{0}/api/files/local".format(SERVER)])
 		if ret == 0:
 			success("Succesfully uploaded '%s' GCODE to Octoprint..."%name)
 		else:
@@ -31,44 +31,119 @@ def upload(gcode):
 		failed("Failed to upload [ex] '%s'... "%gcode)
 	finally:
 		TRASH and trash(gcode)
-
-def run(gcode):
-		if not os.path.exists(gcode):
-			error("Failed to upload [fnf] '%s'..."%gcode)
-			exit(2)
-		if gcode.startswith(DEFAULT_LOCATION):
-			upload(gcode)
-		else:
-			EDITOR and call([EDITOR, gcode])
 		
 if __name__ == '__main__':
 	parser = argparse.ArgumentParser(description='Upload GCODE to Octoprint.', add_help=False)
-	parser.add_argument('-g', '--gcode', required=True)
-	
+	parser.add_argument('--gcode', required=True)
+	parser.add_argument('--key')
+	parser.add_argument('--server')
+	parser.add_argument('--location')
+	parser.add_argument('--editor')
+	parser.add_argument('switches', nargs='*', choices = ["select", "print", "trash", "default"], default="default")
+
 	try:
 		args = parser.parse_args()
-	except:
-		error("Not enough arguments.")
+	except Exception as e:
+		print(e)
+		error("Called with invalid parameters.")
 		exit(1)
+
+	#first thing first...
+	gcode = os.path.expanduser(args.gcode)
+	if not os.path.exists(gcode):
+		error("Failed to upload [fnf] '%s'..."%gcode)
+		exit(2)
+
+	#default settings
+	SERVER = "http://octoprint.local"
+	OCTOPRINT_KEY = None
+	DEFAULT_LOCATION = "~/Desktop"
+	EDITOR = "/usr/local/bin/mate"
+	TRASH = False
+	SELECT = False 
+	PRINT = False 
 	
+	#read settigns from ini file, if available
 	try:
-		config = configparser.ConfigParser()
 		config_file = os.path.join(os.path.expanduser("~"), ".toctoprint.ini")
-		if not os.path.exists(config_file):
-			error("Config file '%s' does not exist."%config_file)
-			exit(2)
-		
-		config.read(config_file)
-		
-		SERVER = config['default']['SERVER']
-		OCTOPRINT_KEY = config['default']['OCTOPRINT_KEY']
-		DEFAULT_LOCATION = os.path.expanduser(config['default']['DEFAULT_LOCATION'])
-		EDITOR = config['default']['EDITOR']
-		TRASH = config['default']['TRASH']
-		
+		if os.path.exists(config_file):
+			config = configparser.ConfigParser()
+			config.read(config_file)
+			
+			try:
+				SERVER = config['default']['SERVER']
+			except:
+				pass
+			try:	
+				OCTOPRINT_KEY = config['default']['OCTOPRINT_KEY']
+			except:
+				pass
+			try:
+				DEFAULT_LOCATION = config['default']['DEFAULT_LOCATION']
+			except:
+				pass
+			try:
+				EDITOR = config['default']['EDITOR']
+			except:
+				pass
+			try:
+				TRASH = config['default']['TRASH']
+			except:
+				pass
+			try:
+				SELECT = config['default']['SELECT']
+			except:
+				pass
+			try:
+				PRINT = config['default']['PRINT']
+			except:
+				pass
 	except Exception as e:
 		error("Error reading configuration.\n%s"%e)
 		exit(3)
+
+	#update settigns based on received parameters
+	if args.server: 
+		SERVER = args.server
+	if args.key:
+		OCTOPRINT_KEY = args.key
+	if args.location:
+		DEFAULT_LOCATION = args.location
+	if args.editor:
+		EDITOR = args.editor
+
+	if SELECT:
+		SELECT = "select=true"
+	else:
+		SELECT = "select=false"
+
+	if PRINT:
+		PRINT = "print=true"
+		SELECT = "select=true"
+	else:
+		PRINT = "print=false"
+		
+	if args.switches != "default":
+		if "print" in args.switches:
+			SELECT = "select=true"
+			PRINT = "print=true"
+
+		if "select" in args.switches:
+			SELECT = "select=true"
+
+		if "trash" in args.switches:
+			TRASH = True
+
+	#can't go on without these 2
+	if OCTOPRINT_KEY == None or SERVER == None : 
+		error("Called with invalid parameters.")
+		exit(4)
 	
-	with daemon.DaemonContext(initgroups=False):	
-		run(args.gcode)
+	DEFAULT_LOCATION = os.path.expanduser(DEFAULT_LOCATION)
+
+	if gcode.startswith(DEFAULT_LOCATION):
+		#start the upload in a background process
+		with daemon.DaemonContext(initgroups=False):	
+			upload(gcode)
+	else:
+		EDITOR and call([EDITOR, gcode])
