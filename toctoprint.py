87fddc08c1bc5911a26722e49d46df1c4b916b5c
ǝuıɥsuooɯ <github@ovidiu.me>
2016-10-13 14:29:02 +0800

    enhanced background process, removed LOCATION

--- toctoprint.py
+++ toctoprint.py
@@ -14,7 +14,7 @@ def error(message):
 	call(["/usr/local/bin/terminal-notifier",  "-message" , str(message), "-title" , "Simplify3D" , "-sound" , "Glass.aiff" , "-sender" , "com.Simplify3D.S3D-Software", "-contentImage",  "/System/Library/CoreServices/CoreTypes.bundle/Contents/Resources/AlertStopIcon.icns"])
 
 def success(message):
-	call(["/usr/local/bin/terminal-notifier",  "-message" , str(message), "-title" , "Simplify3D" , "-sound" , "Glass.aiff" , "-sender" , "com.Simplify3D.S3D-Software", "-contentImage",  "/System/Library/CoreServices/CoreTypes.bundle/Contents/Resources/ToolbarFavoritesIcon.icns"])
+	call(["/usr/local/bin/terminal-notifier",  "-message" , str(message), "-title" , "Simplify3D" , "-sender" , "com.Simplify3D.S3D-Software", "-contentImage",  "/System/Library/CoreServices/CoreTypes.bundle/Contents/Resources/ToolbarFavoritesIcon.icns"])
 
 def trash(filename):
 	call(["/usr/local/bin/trash", filename]) 
@@ -114,102 +114,94 @@ def upload(gcode):
 		TRASH and trash(gcode)
 		
 if __name__ == '__main__':
-	parser = argparse.ArgumentParser(description='Upload GCODE to Octoprint.', add_help=False)
-	parser.add_argument('--gcode', required=True)
-	parser.add_argument('--key')
-	parser.add_argument('--server')
-	parser.add_argument('--location')
-	parser.add_argument('--editor')
-	parser.add_argument('switches', nargs='*', choices = ["select", "print", "trash", "insecure", "default"], default="default")
+	with daemon.DaemonContext(initgroups=False):
+		parser = argparse.ArgumentParser(description='Upload GCODE to Octoprint.', add_help=False)
+		parser.add_argument('--gcode', required=True)
+		parser.add_argument('--key')
+		parser.add_argument('--server')
+		parser.add_argument('--editor')
+		parser.add_argument('switches', nargs='*', choices = ["select", "print", "trash", "insecure", "default"], default="default")
 
-	try:
-		args = parser.parse_args()
-	except Exception as e:
-		print(e)
-		error("Called with invalid parameters.")
-		exit(1)
+		try:
+			args = parser.parse_args()
+		except Exception as e:
+			error("Called with invalid parameters.")
+			exit(1)
 
-	#first thing first...
-	gcode = os.path.expanduser(args.gcode)
-	if not os.path.exists(gcode):
-		error("""Failed to upload [fnf]...
+		#first thing first...
+		gcode = os.path.expanduser(args.gcode)
+		if not os.path.exists(gcode):
+			error("""Failed to upload [fnf]...
 %s"""%gcode)
-		exit(2)
+			exit(2)
 
-	#default settings
-	SERVER = "http://octoprint.local"
-	OCTOPRINT_KEY = None
-	DEFAULT_LOCATION = "~/Desktop"
-	EDITOR = "/usr/local/bin/mate"
-	TRASH = False
-	INSECURE = ""
-	SELECT = "select=false" 
-	PRINT = "print=false"
+		#default settings
+		SERVER = "http://octoprint.local"
+		OCTOPRINT_KEY = None
+		EDITOR = "/usr/local/bin/mate"
+		TRASH = False
+		INSECURE = ""
+		SELECT = "select=false" 
+		PRINT = "print=false"
 	
 	
-	#read settigns from ini file, if available
-	try:
-		config_file = os.path.join(os.path.expanduser("~"), ".toctoprint.ini")
-		if os.path.exists(config_file):
-			config = configparser.ConfigParser()
-			config.read(config_file)
+		#read settigns from ini file, if available
+		try:
+			config_file = os.path.join(os.path.expanduser("~"), ".toctoprint.ini")
+			if os.path.exists(config_file):
+				config = configparser.ConfigParser()
+				config.read(config_file)
 			
-			try:
-				SERVER = config['default']['SERVER']
-			except:
-				pass
-			try:	
-				OCTOPRINT_KEY = config['default']['OCTOPRINT_KEY']
-			except:
-				pass
-			try:
-				DEFAULT_LOCATION = config['default']['DEFAULT_LOCATION']
-			except:
-				pass
-			try:
-				EDITOR = config['default']['EDITOR']
-			except:
-				pass
-	except Exception as e:
-		error("Error reading configuration.\n%s"%e)
-		exit(3)
+				try:
+					SERVER = config['default']['SERVER']
+				except:
+					pass
+				try:	
+					OCTOPRINT_KEY = config['default']['OCTOPRINT_KEY']
+				except:
+					pass
+				try:
+					EDITOR = config['default']['EDITOR']
+				except:
+					pass
+		except Exception as e:
+			error("""Error reading configuration.
+%s"""%e)
+			exit(3)
 
-	#update settigns based on received parameters
-	if args.server: 
-		SERVER = args.server
-	if args.key:
-		OCTOPRINT_KEY = args.key
-	if args.location:
-		DEFAULT_LOCATION = args.location
-	if args.editor:
-		EDITOR = args.editor
+		#update settigns based on received parameters
+		if args.server: 
+			SERVER = args.server
+		if args.key:
+			OCTOPRINT_KEY = args.key
+		if args.editor:
+			EDITOR = args.editor
 
-	if args.switches != "default":
-		if "print" in args.switches:
-			SELECT = "select=true"
-			PRINT = "print=true"
+		if args.switches != "default":
+			if "print" in args.switches:
+				SELECT = "select=true"
+				PRINT = "print=true"
 
-		if "select" in args.switches:
-			SELECT = "select=true"
+			if "select" in args.switches:
+				SELECT = "select=true"
 
-		if "trash" in args.switches:
-			TRASH = True
+			if "trash" in args.switches:
+				TRASH = True
 
-		if "insecure" in args.switches:
-			INSECURE = "--insecure"
+			if "insecure" in args.switches:
+				INSECURE = "--insecure"
 
-	#can't go on without these 2
-	if OCTOPRINT_KEY == None or SERVER == None : 
-		error("Missing server information.")
-		exit(4)
-	
-	DEFAULT_LOCATION = os.path.expanduser(DEFAULT_LOCATION)
+		#can't go on without these 2
+		if OCTOPRINT_KEY == None or SERVER == None : 
+			error("Missing server information.")
+			exit(4)
 
-	if gcode.startswith(DEFAULT_LOCATION) and not os.path.basename(gcode).startswith("_"):
-		if os.path.basename(gcode).startswith(" "):
-			call([EDITOR, "-w",  gcode])
-		#start the upload in a background process
-		with daemon.DaemonContext(initgroups=False):
+		if os.path.basename(gcode).startswith("_"):
+			EDITOR and call([EDITOR, "-w",  gcode])
+			trash(gcode)
+		else:
+			if os.path.basename(gcode).startswith(" "):
+				call([EDITOR, "-w",  gcode])
 			upload( get_renamed(gcode) )
-	else:
-		EDITOR and call([EDITOR, gcode])
+
+
